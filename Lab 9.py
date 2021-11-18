d44a269ae37a8c107cc484c036ac4217470deb64
anthonydelvecchio <amdelvecchio@loyola.edu>
2021-11-18 10:40:01 -0500

    work done in class, will find partner soon

---
+++ Lab 9.py
@@ -0,0 +1,10 @@
+# Programmers: Anthony DelVecchio
+# Course: CS151, Dr. Rajeev
+# Date: 11/18/21
+# Lab Number: 9
+# Program Inputs: CSV Name
+# Program Outputs: Minimum and maximum profit amongst all movies
+
+# Define function to load movie data
+def load_movie_data(filename):
+    filename = input("Input File Name: ")
